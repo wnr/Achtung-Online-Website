693d7bf07b46c40e1a9d854bb9a15a4c9904bce5
Mathias Lindblom <mathias.lindblom@trustly.se>
2013-11-03 19:21:58 +0100

    First version with similar style as the game

--- head.php
+++ head.php
@@ -1,3 +1,5 @@
+
+
 <?php
 
 function printHead($root = "./", $js = true){
@@ -10,40 +12,23 @@ function printHead($root = "./", $js = true){
     <title>Achtung Online</title>
     
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    
-    <link rel="stylesheet" href="'.$root.'css/style.css" type="text/css">';
-    
-    
-    if($js){
-        echo '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-    <script type="text/javascript">
-    if (typeof jQuery == "undefined") {
-        document.write(unescape("%3Cscript src=' . $root . "'/js/jquery-1.6.2.min.js'" . $root . "type='text/javascript'" . '%3E%3C/script%3E"));
-    }
-    </script>
-    
-    <script type="text/javascript" src="'.$root.'js/jquery.placeholder.js"></script>
-    <script type="text/javascript" src="'.$root.'js/jquery.bgstretcher.js"></script>
-    
-    <script type="text/javascript" src="'.$root.'js/app.js"></script>
-    ';
-    }
-    
-    echo '<script type="text/javascript">
-        var _gaq = _gaq || [];
-     _gaq.push(["_setAccount", "UA-24270170-1"]);
-     _gaq.push(["_setDomainName", "none"]);
-     _gaq.push(["_setAllowLinker", true]);
-     _gaq.push(["_trackPageview"]);
-   
-     (function() {
-       var ga = document.createElement("script"); ga.type = "text/javascript"; ga.async = true;
-       ga.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
-       var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ga, s);
-     })();
-    </script>
-</head>';
-
-}
-
+    <link href="'.$root.'bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="'.$root.'jumbotron.css" rel="stylesheet">
+    <link href="'.$root.'css/stylesheet.css" rel="stylesheet">
+</head>
+<body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a style="margin-left: -5px;" class="navbar-brand" href="/">Home</a>
+          <a class="navbar-brand" href="game/">Play</a>
+          <a class="navbar-brand" href="https://www.facebook.com/achtungonline">Community</a>
+          <iframe id = "facebook_frame"src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Ffacebook.com/achtungonline&amp;layout=button_count&amp;show_faces=true&amp;action=like&amp;colorscheme=light" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150px; height:30px; margin-top: 15px;" allowTransparency="true" ></iframe>
+      </div>
+    </div>';
+} 
 ?>
--- index.php
+++ index.php
@@ -2,32 +2,59 @@
 require("head.php");
 
 printHead();
-
-require("upperbody.php");
+require("jumbotron_header.php");
 ?>
-
-<div id="text">
-    <h1>OPEN BETA</h1>
-    <p>
-        Welcome to Achung Online! This is the game for you that seeks a simple, addictive and yet extremely fun game to challange friends with.
-        Gather around with your favourite snacks and explore the randomness of the newly created version of the well known game "achtung die kurve".
-        Not only have we created crazy powerups and game modes - we have also added a lot of awesesomeness into the game!
-        
-    </p>
-    <p>
-        The game is in an early beta stage and can therefore be somewhat unpredictable.
-        Please report any faulties that you come across.
-    </p>
-    <p>
-        Any questions? Wanna get in touch with us? Feedback?
-        <br />
-        Make sure you check out our <a href="forum/">FORUM</a>. We would love to hear from you!
-    </p>
-    <div class="line"></div>
-    <a class="big_link" href="game/">CLICK HERE TO PLAY THE GAME</a>
-</div>
+<h1 style="margin-top:33px">Achtung Online</h1> 
+<p>Welcome to Achtung Online! This is the game for you that seeks a simple, addictive and yet extremely fun game to challange friends with. Gather around with your favourite snacks and explore the randomness of the newly created version of the well known game "achtung die kurve". Not only have we created crazy powerups and game modes - we have also added a lot of awesesomeness into the game! </p>
+<a id="start_game_button" href="game/">
+  <img src="img/btn_dark.png" />
+</a>
+<?php require("jumbotron_footer.php");?>
 
 
-<?php
-require("lowerbody.php");
-?>
+<div class="container" id="body_container">
+  <!-- Example row of columns -->
+  <div class="row" >
+    <div class="col-md-4">
+      <img data-src="holder.js/400*300" class="img-thumbnail" alt="400*300" style="width: 400px; height: 300px;" src="img/gameplay_square.png">
+      <h2>Customizability</h2>
+      <p>We want you to decide how you wanna play the game. You can experience the classical Achtung Die Kurve simply by removing the powerups and even the holes/jumps. If you don´t like the default feeling of the game you can change the speed, turning speed, jump frequency and more. You can also remove a few of the powerups if you don´t like them. If this is to much for you then just screw the settings and play the game!
+        <br><br>
+        We will continue to develop Achtung Online and would love to hear your thoughts on what features you would like us to implement and what bugs you have experienced (hopefully none). To do this just visit our <a href="https://www.facebook.com/achtungonline">community page</a>.</p>
+      </div>
+      <div class="col-md-4">
+
+        <img data-src="holder.js/400*300" class="img-thumbnail" alt="400*300" style="width: 400px; height: 300px;" src="img/gameplay_circle.png">
+        <h2>Different maps</h2>
+        <p>If you get bored with the classical square map try out our circle map. But be aware that the wallhack powerup might be hard to master in this mind botteling mirror map.
+          <br><br>
+          In the future we will add more map types of different sizes and shapes. If you have any request you are most welcome to post it (or vote it up if it exist) in our <a href="https://www.facebook.com/achtungonline">community page</a>.</p>
+        </div>
+        <div class="col-md-4">
+
+          <img data-src="holder.js/400*300" class="img-thumbnail" alt="400*300" style="width: 400px; height: 300px;" src="img/gameplay_options.png">
+          <h2>Teamplay or FFA</h2>
+          <p>Play FFA up to 12 players or team up against each other. You can play the clasical 2 vs 2 or you can play 4 vs 3 vs 2 vs 1. You decide! Don´t forget that you can change the "Target Score" in settings if the default don´t suite you and your friends.</p>
+        </div>
+      </div>
+
+      <?php require("main_footer.php");?>
+
+    </div> <!-- /container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+
+  <script>
+  $('#start_game_button img').hover(function() {
+    $(this).attr('src', 'img/btn_light.png');
+  }, function() {
+    $(this).attr('src', 'img/btn_dark.png');
+  }).mousedown(function() {
+    $(this).attr('src', 'img/btn_pressed.png');
+  });
+  </script>
